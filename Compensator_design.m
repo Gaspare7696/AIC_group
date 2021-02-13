c4cbb0cf2501e1b004560534fa77705c6f094dae
angelocarnazzo <angelo.carnazzo97@gmail.com>
2021-02-13 18:49:41 +0100

    layout compensator

--- Compensator_design.m
+++ Compensator_design.m
@@ -1,15 +1,38 @@
+%%%SCHEMATIC%%%
+
 gm1=142.5e-6;
 gm2=390.1e-6;
 gm3=36.77e-3;
 gmf=12.12e-3;
+
 CL=500e-12;
+GBW=1.5e6;
 
 %If we want wGBW=1.5MHz:
 
-Cc1= 15e-12 
+Cc1=gm1/(GBW*2*pi)
 
 Rc1=1/gm3
 
 Cc2=Cc1*gm2/(gm3-gm2)
 
 Rc2=CL/(gm3*Cc2) - gmf/(gm3*gm2)
+
+%%%LAYOUT%%%
+
+gm1_L=77.6e-6;
+gm2_L=207.8e-6;
+gm3_L=36.77e-3;
+gmf_L=11.99e-3;
+
+Cc1_L=gm1_L/(GBW*2*pi)
+
+Rc1_L=1/gm3_L
+
+Cc2_L=Cc1_L*gm2_L/(gm3_L-gm2_L)
+
+Rc2_L=CL/(gm3_L*Cc2_L) - gmf_L/(gm3_L*gm2_L)
+
+Co2_L=1.09e-12+65.42e-15 %%Cgs10//Cds8
+
+verification=Cc2_L/((gm1_L*gm2_L/((gm3_L-gm2_L)^2))*Co2_L)
